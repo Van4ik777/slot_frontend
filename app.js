e0f1e2ff425608babf5c0a9012f3e5b21fdc6301
Vanchik777 <lynnyk.first@gmail.com>
2024-07-11 19:09:42 +0300

    second

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 async function spin(){
-    let res = await fetch('http://127.0.0.1:8000/api/spin/')
+    let res = await fetch('http://127.0.0.1:8000/api/spin/',
+    {
+      Method: 'POST',
+      Headers: {
+        'Content-Type': 'application/json'
+      },
+      Body: JSON.parse({initial_money: 1000, stavka: 100}),
+    }
+    )
     let data = res.json()
     return data
 }
