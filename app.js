050dabc1406bfed70a6f8faac9deee799387759b
Vanchik777 <lynnyk.first@gmail.com>
2024-07-11 19:21:09 +0300

    second111

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
-async function spin(){
+let form = document.querySelector('form')
+let input = document.querySelector('input')
+let currSpin
+
+async function spin(initial_money,stavka){
     let res = await fetch('http://127.0.0.1:8000/api/spin/',
     {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({initial_money: 1000, stavka: 100}),
+      body: JSON.stringify({initial_money, stavka}),
     }
     )
     let data = res.json()
     return data
 }
-spin().then(data=>console.log(data))
+form.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    spin(1000,+input.value).then(data=>{
+    currSpin = data
+    console.log(currSpin)
+    })
+
+})
