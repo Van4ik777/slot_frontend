efc29311cf3d32323e537f06ba7dbf04c43a17ed
Vanchik777 <lynnyk.first@gmail.com>
2024-07-11 19:23:37 +0300

    second1111

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 let form = document.querySelector('form')
 let input = document.querySelector('input')
 let currSpin
-
-async function spin(initial_money,stavka){
+let startMoney= 1000
+async function spin(initial_money, stavka){
     let res = await fetch('http://127.0.0.1:8000/api/spin/',
     {
       method: 'POST',
@@ -17,8 +17,9 @@ async function spin(initial_money,stavka){
 }
 form.addEventListener('submit',(e)=>{
     e.preventDefault()
-    spin(1000,+input.value).then(data=>{
+    spin(startMoney,+input.value).then(data=>{
     currSpin = data
+    startMoney= data.money
     console.log(currSpin)
     })
 
