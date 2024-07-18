57ce55d4fda528664b8f40f1bee5fb09034149b6
user <lynnyk.first@gmail.com>
2024-07-18 21:32:17 +0300

    log

--- app.js
+++ app.js
@@ -65,9 +65,7 @@ async function spin(initial_money, stavka) {
 async function register() {
     let res = await fetch('http://127.0.0.1:8000/api/register/', {
         method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
+
         body: JSON.stringify({
             'username': 'vania',
             'password': 'newpassword'
